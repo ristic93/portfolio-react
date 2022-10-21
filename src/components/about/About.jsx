4f14075ef03843331682bde36c410e4a2d86dc30
ristic93 <risticbro@gmail.com>
2022-10-21 17:49:52 +0200

    added new project

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -15,8 +15,8 @@ const about = () => {
                 <h3>About me</h3>
         <article className="container about_container">
             <div className="text">
-                <h4>Few words about me</h4>
-                <p style={{fontSize: '1.1rem'}}>Ambitious, hard-working and organized. Communicative, creative and resourceful. Great problem-solving skills. Reliable team player who is able to adapt to new surroundings <FcPortraitMode className='about_icons'/>. </p>
+                <h4>A few words about me</h4>
+                <p style={{fontSize: '1.1rem'}}>Ambitious, hard-working and well organized. Communicative, creative and resourceful. Great problem-solving skills. Reliable team player who is able to adapt to new surroundings <FcPortraitMode className='about_icons'/>. </p>
                 <h4>How I can contribute</h4>
                 <p style={{fontSize: '1.1rem'}}>I develop websites with HTML, CSS and JavaScript <FcMultipleDevices className='about_icons'/>. I love taking complex problems and turning them into simple <FcIdea className='about_icons'/>. I also love the logic and structure of coding and always strive to write elegant and efficient code whether it be HTML, CSS or JavaScript <FcMindMap className='about_icons'/>. Currently I am focused on learning React <FaReact style={{color: 'lightblue', fontSize: '1.4rem'}}/>, so I would like to raise my knowledge to a higher level as well as improving knowledge in existing ones <FcPuzzle className='about_icons'/>.</p>
                 <h4>What are my goals</h4>
--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -7,6 +7,7 @@ import img4 from '../../assets/e-commerce.png';
 import img5 from '../../assets/CRUD-app.png';
 import img6 from '../../assets/digital-clock.png';
 import img7 from '../../assets/weather-app.png';
+import img8 from '../../assets/budget-app.png';
 import {FcRightDown2} from 'react-icons/fc';
 import {FcApproval} from 'react-icons/fc';
 
@@ -15,57 +16,65 @@ const Projects = () => {
   const data = [
     {
       id: 1,
+      image: img8,
+      title: 'Budget app (React)',
+      desc: 'In order to make this app i worked with hooks: useState, useEffect, useContext and useReduce. Solved income and expense amounts by storing data in localstorage for XY days. Learned how to work with Context and Reducer.',
+      github: 'https://github.com/ristic93/react-budget-app',
+      live: 'https://budget-app-2022.netlify.app'
+    },
+    {
+      id: 2,
       image: img1,
       title: 'Shopping list (React)',
-      desc: 'Simple shopping list made by using React.js',
+      desc: 'Simple shopping list made by using HTML5, CSS3, JavaScript, React.js. Also used hooks like useState, useEffect, and useRef in order to make it.',
       github: 'https://github.com/ristic93/React-shopping-list',
       live: 'https://loquacious-frangollo-e85cf6.netlify.app'
     },
     {
-      id: 2,
+      id: 3,
       image: img2,
       title: 'Digital menu',
-      desc: 'Digital menu for a fast food bar - Kod Brku. Made with HTML, SASS & JavaScript. Made for displays like smart phones and tablets.',
+      desc: 'Digital menu for a fast food bar - Kod Brku. Made by using HTML, SASS & JavaScript. This project is made for displays like tablets and smartphones. Made for a client, and made together with my colleague Nikola. QR code is scanned 100+ times every day.',
       github: 'https://github.com/ristic93/Digital-Menu-Kod-brku',
       live: 'https://www.kodbrku.rs'
     },
     {
-      id: 3,
+      id: 4,
       image: img3,
       title: 'Digital menu',
-      desc: 'Digital menu for a caffe bar - River-Caffe. Made with HTML, SASS & JavaScript. Made for displays like smart phones and tablets.',
+      desc: 'Digital menu for a caffe bar - River-Caffe. Made by using HTML, SASS & JavaScript. This project is made for displays like tablets and smartphones. Made for a client, and made together with my colleague Nikola. QR code is scanned 100+ times every day.',
       github: 'https://github.com/ristic93/Digital-Menu-River-Caffe',
       live: 'http://river-caffe.rs/'
     },
     {
-      id: 4,
+      id: 5,
       image: img4,
       title: 'Ecommerce',
-      desc: 'Simple WebShop app made by using HTML5, CSS3, Bootstrap 5 and Javascript with Fetch API.',
+      desc: 'Simple WebShop app made by using HTML5, CSS3, Bootstrap 5 and Javascript with Fetch API. Also worked with session and local storage in order to save state of orders in shopping cart.',
       github: 'https://github.com/ristic93/JS-Ecommerce',
       live: 'https://mellow-crostata-c3ca2e.netlify.app/'
     },
     {
-      id: 5,
+      id: 6,
       image: img5,
       title: 'CRUD app',
-      desc: 'Made by using HTML5, CSS3, Bootstrap 5 and Javascript.',
+      desc: 'Made by using HTML5, CSS3, Bootstrap 5 and Javascript. Learned how to manipulate with DOM elements. Create, read, update and delete methods.',
       github: 'https://github.com/ristic93/Basic-CRUD-app',
       live: 'https://cosmic-bonbon-8d8e2e.netlify.app/'
     },
     {
-      id: 6,
+      id: 7,
       image: img6,
       title: 'Digital clock',
-      desc: 'Made by using HTML5, CSS3 and Javascript.',
+      desc: 'Made by using HTML5, CSS3 and Javascript. Learned how to manipulate with DOM elements and Date Objects.',
       github: 'https://github.com/ristic93/JS-Digital-Clock',
       live: 'https://gentle-kitsune-17f053.netlify.app'
     },
     {
-      id: 7,
+      id: 8,
       image: img7,
       title: 'Weather app',
-      desc: 'Simple weather app made by using HTML5, CSS3 and Javascript with Fetch API',
+      desc: 'Simple weather app that is showing temperature based on your current location. Made by using HTML5, CSS3 and Javascript with Fetch API.',
       github: 'https://github.com/ristic93/JS-Weather-app',
       live: 'https://superlative-kleicha-a80af8.netlify.app'
     },
